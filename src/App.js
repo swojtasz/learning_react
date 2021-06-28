fc64364a5738bc315d2352f8bb3eb8e406b4509b
swojtasz <szymon.wojtaszczyk1@gmail.com>
2021-06-28 19:48:02 +0200

    starting point

--- src/App.js
+++ src/App.js
@@ -1,20 +1,8 @@
-import { useContext } from "react";
-
-import Login from "./components/Login/Login";
-import Home from "./components/Home/Home";
-import MainHeader from "./components/MainHeader/MainHeader";
-import AuthContext from "./store/auth-context";
-
 function App() {
-  const ctx = useContext(AuthContext);
   return (
-    <>
-      <MainHeader />
-      <main>
-        {!ctx.isLoggedIn && <Login />}
-        {ctx.isLoggedIn && <Home />}
-      </main>
-    </>
+    <div>
+      <h2>Let's get started!</h2>
+    </div>
   );
 }
 
