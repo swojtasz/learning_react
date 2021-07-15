7608973b3368a6c51d14a06371a15d05ddc7f397
swojtasz <szymon.wojtaszczyk1@gmail.com>
2021-07-15 19:14:29 +0200

    start

--- src/App.js
+++ src/App.js
@@ -1,47 +1,10 @@
-import React, { useEffect, useState } from "react";
-
-import Tasks from "./components/Tasks/Tasks";
-import NewTask from "./components/NewTask/NewTask";
-import useHttp from "./hooks/use-http";
+import SimpleInput from './components/SimpleInput';
 
 function App() {
-  const [tasks, setTasks] = useState([]);
-
-  const { error, isLoading, sendRequest: fetchTasks } = useHttp();
-
-  useEffect(() => {
-    const transformTasks = (tasks) => {
-      const loadedTasks = [];
-
-      for (const taskKey in tasks) {
-        loadedTasks.push({ id: taskKey, text: tasks[taskKey].text });
-      }
-
-      setTasks(loadedTasks);
-    };
-
-    fetchTasks(
-      {
-        url: "https://learning-react-1e99a-default-rtdb.europe-west1.firebasedatabase.app/tasks.json",
-      },
-      transformTasks
-    );
-  }, [fetchTasks]);
-
-  const taskAddHandler = (task) => {
-    setTasks((prevTasks) => prevTasks.concat(task));
-  };
-
   return (
-    <React.Fragment>
-      <NewTask onAddTask={taskAddHandler} />
-      <Tasks
-        items={tasks}
-        loading={isLoading}
-        error={error}
-        onFetch={fetchTasks}
-      />
-    </React.Fragment>
+    <div className="app">
+      <SimpleInput />
+    </div>
   );
 }
 
