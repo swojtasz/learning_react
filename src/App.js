5b29214c85101c5ac696dfde00870b1383c74acf
swojtasz <szymon.wojtaszczyk1@gmail.com>
2021-07-24 17:32:08 +0200

    finished

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
+import { Redirect, Route, Switch } from "react-router-dom";
+import AllQuotes from "./pages/AllQuotes";
+import NewQuote from "./pages/NewQuote";
+import QuoteDetails from "./pages/QuoteDetails";
+import Layout from "./components/layout/Layout";
+import NotFound from "./pages/NotFound";
+
 function App() {
   return (
-    <div>
-
-    </div>
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="quotes" />
+        </Route>
+        <Route path="/quotes" exact>
+          <AllQuotes />
+        </Route>
+        <Route path="/quotes/:quoteId">
+          <QuoteDetails />
+        </Route>
+        <Route path="/new-quote">
+          <NewQuote />
+        </Route>
+        <Route path="*">
+          <NotFound />
+        </Route>
+      </Switch>
+    </Layout>
   );
 }
 
--- src/components/quotes/QuoteForm.js
+++ src/components/quotes/QuoteForm.js
@@ -1,10 +1,12 @@
-import { useRef } from 'react';
+import { useRef, useState } from "react";
+import { Prompt } from "react-router-dom";
 
-import Card from '../ui/Card';
-import LoadingSpinner from '../ui/LoadingSpinner';
-import classes from './QuoteForm.module.css';
+import Card from "../UI/Card";
+import LoadingSpinner from "../UI/LoadingSpinner";
+import classes from "./QuoteForm.module.css";
 
 const QuoteForm = (props) => {
+  const [wasFocused, setWasFocused] = useState(false);
   const authorInputRef = useRef();
   const textInputRef = useRef();
 
@@ -19,28 +21,48 @@ const QuoteForm = (props) => {
     props.onAddQuote({ author: enteredAuthor, text: enteredText });
   }
 
-  return (
-    <Card>
-      <form className={classes.form} onSubmit={submitFormHandler}>
-        {props.isLoading && (
-          <div className={classes.loading}>
-            <LoadingSpinner />
-          </div>
-        )}
+  const formFocusedHandler = () => {
+    setWasFocused(true);
+  };
 
-        <div className={classes.control}>
-          <label htmlFor='author'>Author</label>
-          <input type='text' id='author' ref={authorInputRef} />
-        </div>
-        <div className={classes.control}>
-          <label htmlFor='text'>Text</label>
-          <textarea id='text' rows='5' ref={textInputRef}></textarea>
-        </div>
-        <div className={classes.actions}>
-          <button className='btn'>Add Quote</button>
-        </div>
-      </form>
-    </Card>
+  const finishEnteringHandler = () => {
+    setWasFocused(false);
+  };
+
+  return (
+    <>
+      <Prompt
+        when={wasFocused}
+        message={(location) => "Are you sure you want to leave?"}
+      />
+      <Card>
+        <form
+          onFocus={formFocusedHandler}
+          className={classes.form}
+          onSubmit={submitFormHandler}
+        >
+          {props.isLoading && (
+            <div className={classes.loading}>
+              <LoadingSpinner />
+            </div>
+          )}
+
+          <div className={classes.control}>
+            <label htmlFor="author">Author</label>
+            <input type="text" id="author" ref={authorInputRef} />
+          </div>
+          <div className={classes.control}>
+            <label htmlFor="text">Text</label>
+            <textarea id="text" rows="5" ref={textInputRef}></textarea>
+          </div>
+          <div className={classes.actions}>
+            <button onClick={finishEnteringHandler} className="btn">
+              Add Quote
+            </button>
+          </div>
+        </form>
+      </Card>
+    </>
   );
 };
 
